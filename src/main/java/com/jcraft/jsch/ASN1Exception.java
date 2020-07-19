3e48c4f4bfc794c3120869f37f42d438b22adfb6
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2020-07-19 20:32:43 +0200

    New file

---
+++ src/main/java/com/jcraft/jsch/ASN1Exception.java
@@ -0,0 +1,8 @@
+package com.jcraft.jsch;
+
+/* This module is a bit small, but for some compatibility reason it cannot be a nested class */
+public class ASN1Exception extends Exception{
+  public ASN1Exception(String s){
+    super(s);
+  }
+}
